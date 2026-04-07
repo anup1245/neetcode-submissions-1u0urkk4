f4d1b0b174cb052877bce395ca16cf875b54c139
anup1245 <anup1245@gmail.com>
2026-04-06 23:16:34 -0700

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.kt
@@ -0,0 +1,41 @@
+class Solution {
+    fun minWindow(s: String, t: String): String {
+        var left = 0
+        var right = 0
+        var max = Integer.MAX_VALUE
+        val map = HashMap<Char, Int>()
+        val window = HashMap<Char, Int>()
+        var match = 0
+        var result = ""
+        for(c in t) {
+            map[c] = map.getOrDefault(c, 0) + 1
+        }
+
+        while(right < s.length) {
+            val c = s[right]
+            if(map.containsKey(c)) {
+                window[c] = window.getOrDefault(c, 0) + 1
+                if(window[c]!! == map[c]!!) match++
+            }
+
+            while(match == map.size) {
+                if(right - left + 1 < max) {
+                    max = right - left + 1
+                    result = s.substring(left, right +1)
+                }
+
+                val c = s[left]
+               if(window.containsKey(c)) {
+                    window[c] = window.getOrDefault(c, 0) - 1
+                    if(window[c]!! < map[c]!!) match--
+                    if(window[c]!! == 0) {
+                        window.remove(c)
+                    }
+               }
+               left++
+            }
+            right++
+        }
+        return result
+    }
+}
