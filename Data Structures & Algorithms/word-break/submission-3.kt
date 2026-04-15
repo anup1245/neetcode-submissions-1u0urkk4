4a2b443842e9d01324e4be6041d2d3e649dd733e
anup1245 <anup1245@gmail.com>
2026-04-15 12:39:30 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        val array = BooleanArray(s.length + 1)
+        array[s.length] = true
+
+        for (i in s.length - 1 downTo 0) {
+            for (word in wordDict) {
+                if (i + word.length <= s.length && s.substring(i, i + word.length) == word) {
+                    array[i] = array[i + word.length]
+                }
+                if (array[i]) break
+            }
+        }
+        return array[0]
+    }
+}
