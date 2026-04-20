533668466d3cb18889140e737ad9c2b4446a3245
anup1245 <anup1245@gmail.com>
2026-04-19 18:28:36 -0700

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        var result = 0
+        for (n in nums) {
+            result = result xor n
+        }
+        return result
+    }
+}
